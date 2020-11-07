726e6084a936643779ff892a93cb453d9523b443
Ryan <redmonds@hmc.edu>
2020-11-07 01:50:38 -0800

    created templates for potential functions

--- ContactTracer.py
+++ ContactTracer.py
@@ -8,12 +8,45 @@ class ContactTracer:
     the progression of time'''
 
     def __init__(self):
-        self.userList = []      #list containing every registered person
-        self.day = 0            #calendar currently only tracks number of days
+        '''
+        TODO: could eventually incorporate actual calendar system?
+        '''
+        self.userList = []        #list containing every registered person
+        self.day = 0              #calendar currently only tracks number of days
+        self.incubationPeriod = 3 #If symptoms are reported, trace all contacts from up to this number of days in the past
+                                  #Must be between 0 and 6
+        self.lifespan =  7        #If user goes this long without reporting systems, can go back to being healthy
 
     def __repr__(self):
-        output = 'Today is ' + str(self.day) + ".\n There are currently " + str(size(self.userList)) + " users:\n\n"
+        output = 'Today is ' + str(self.day) + ".\n There are currently " + str(len(self.userList)) + " users:\n\n"
         for user in self.userList:
             output += repr(user) + '\n\n'
         return output
 
+    def traceInteractions(self, user, days):
+        """
+        TODO:
+        traceInteractions will take in an Infected or Potentially Infected
+        user "user", and will track all interactions made by said user within
+        the given amount of time. For each other person found, update them to be
+        Potentially Infected and recursively track their interactions
+
+        Potential issue: since we only operate one day at a time, some people
+        will be falsely tagged due to interactions happening at different times
+        of day
+        """
+        pass
+
+    def newDay(self):
+        """
+        TODO:
+        increment self.day by 1
+
+        make space in each user's interactions list for a new day of interactions
+
+        for any users currently or potentially infected, trace their interactions for that day
+
+        for any potentially infected users who were last potentially infected
+        longer ago than self.lifespan, set to Healthy
+        """
+        pass
--- User.py
+++ User.py
@@ -11,15 +11,30 @@ class User:
     reporting symptoms'''
 
     def __init__(self, name):
-        '''name is a string representing the user's name'''
+        '''
+        name is a string representing the user's name
+
+        Users are expected to register when healthy, and should immediately
+        declare symptoms if not
+        '''
         self.name = name
         self.status = 'Healthy'
         #For space purposes, only track interactions over the past week
+        #Day of the week represented in modulo 7
         self.interactions = [[]]*7
         self.group = OurTracer
+        #potentialInfectionDay exists so that potentiall infected users
+        #can eventually be cleared
+        self.potentialInfectionDay = 0
+        #infectionGeneration indicates how "far" the user is from an infected individual
+        #the higher the number, the lower the chance of an infection
+        self.infectionGeneration = 0
 
     def __repr__(self):
-        return self.name + ': ' + str(self.totalInteractions()) + ' with ' + str(self.uniqueInteractions()) + ' different people\n'
+        '''
+        TODO: custom descriptions for potentially infected individuals depending on the generation?
+        '''
+        return self.name + 'is currently ' + self.status + ':\n They have had' + str(self.totalInteractions()) + ' with ' + str(self.uniqueInteractions()) + ' different people.'
 
 
     def totalInteractions(self):
@@ -32,3 +47,19 @@ class User:
                 if person not in uniquePeople:
                     uniquePeople += [person]
         return len(uniquePeople)
+
+    def interact(self, users):
+        """
+        TODO:
+        users should be a list of other Users
+        for each member of users list, add them to self's interactions
+        for the current day, and vice versa
+        """
+        pass
+
+    def reportSymptoms(self):
+        """
+        TODO:
+        Update the user to being Infected, and trace their recent interactions
+        """
+        pass

b85d647d83c44c98405caa8c38380fc1cfcf169a
Ryan <redmonds@hmc.edu>
2020-11-07 20:12:47 -0800

    consolidated to one file

--- ContactTracer.py
+++ ContactTracer.py
@@ -1,4 +1,3 @@
-from User import User
 
 class ContactTracer:
     '''ContactTracer will keep a list of every person currently using the tracer, and is responsible
@@ -18,10 +17,13 @@ class ContactTracer:
         self.lifespan =  7        #If user goes this long without reporting systems, can go back to being healthy
 
     def __repr__(self):
-        output = 'Today is ' + str(self.day) + ".\n There are currently " + str(len(self.userList)) + " users:\n\n"
+        output = 'Today is the day' + str(self.day) + ".\nThere are currently " + str(len(self.userList)) + " users:\n\n"
         for user in self.userList:
             output += repr(user) + '\n\n'
         return output
+
+
+
 
     def traceInteractions(self, user, days):
         """
@@ -48,5 +50,71 @@ class ContactTracer:
 
         for any potentially infected users who were last potentially infected
         longer ago than self.lifespan, set to Healthy
+        """
+        pass
+
+OurTracer = ContactTracer()
+
+class User:
+    '''Each user stores their recent interactions and their infection
+    level: Healthy, Potentially Infected, or Infected
+    
+    User handles user-end functions such as logging interactions and
+    reporting symptoms'''
+
+    def __init__(self, name):
+        '''
+        name is a string representing the user's name
+
+        Users are expected to register when healthy, and should immediately
+        declare symptoms if not
+        '''
+        self.name = name
+        self.status = 'Healthy'
+        #For space purposes, only track interactions over the past week
+        #Day of the week represented in modulo 7
+        self.interactions = [[]]*7
+        self.group = OurTracer
+        #potentialInfectionDay exists so that potentiall infected users
+        #can eventually be cleared
+        self.potentialInfectionDay = 0
+        #infectionGeneration indicates how "far" the user is from an infected individual
+        #the higher the number, the lower the chance of an infection
+        self.infectionGeneration = 0
+        OurTracer.userList += [self]
+
+
+
+    def __repr__(self):
+        '''
+        TODO: custom descriptions for potentially infected individuals depending on the generation?
+        '''
+        return self.name + ' is currently ' + self.status + ':\n They have had ' + str(self.totalInteractions()) + ' interactions with ' + str(self.uniqueInteractions()) + ' different people.'
+
+
+    def totalInteractions(self):
+        return sum(len(x) for x in self.interactions)
+
+    def uniqueInteractions(self):
+        uniquePeople = []
+        for day in self.interactions:
+            for person in day:
+                if person not in uniquePeople:
+                    uniquePeople += [person]
+        return len(uniquePeople)
+
+    def interact(self, users):
+        """
+        TODO:
+        users should be a list of other Users
+        for each member of users list, add them to self's interactions
+        for the current day, and vice versa
+        """
+        pass
+
+    def reportSymptoms(self):
+        """
+        TODO:
+        Update the user to being Infected, and trace their recent interactions
         """
         pass
